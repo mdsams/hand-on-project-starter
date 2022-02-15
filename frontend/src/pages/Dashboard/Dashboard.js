d3be95cd717131a81583dd8c2b695e093e609a28
mdsams <tabrejmdsams786@gmail.com>
2022-02-15 21:10:37 +0530

    again a problem

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -1,43 +1,25 @@
-// import React, { Component } from "react";
-// // import Header from "../../components/Header/Header";
-
-// import Login from "../../components/Login/login";
-// class Loggedin extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <Login />
-//       </div>
-//     );
-//   }
-// }
-
-// export default Loggedin;
-
-import React, { Component } from "react";
+import React from "react";
 import Main from "../../images/main.svg";
 import Api from "../../components/Api/Api";
 import "./Dashboard.css";
 
-class Dashboard extends Component {
-  render() {
-    return (
-      <div className="main">
-        <img src={Main} alt="Main" id="main" />
+function Dashboard() {
+  return (
+    <div className="main">
+      <img src={Main} alt="Main" id="main" />
 
-        <div className="container">
-          <Api />
-          <Api />
-          <Api />
-          <Api />
-          <Api />
-          <Api />
-          <Api />
-          <Api />
-        </div>
+      <div className="container">
+        <Api />
+        <Api />
+        <Api />
+        <Api />
+        <Api />
+        <Api />
+        <Api />
+        <Api />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Dashboard;
--- frontend/src/pages/LoggedOut/LoggedOut.js
+++ frontend/src/pages/LoggedOut/LoggedOut.js
@@ -4,44 +4,47 @@ import Avatar from "../../images/avatar.svg";
 
 function LoggedOut() {
   return (
-    <React.Fragment>
-      <div className="container">
-        <div className="rectangleBox">
-          <div className="rightElement">
-            <img src={Avatar} alt="Avatar" id="avatar" />
-            <h1>Welcome To Your Dashboard</h1>
-            <h2>
-              Your uploaded APIs will be displayed here once you login to your
-              account
-            </h2>
-          </div>
-        </div>
-        <div className="form">
-          <form>
-            <div className="formElement">
-              <text id="text">Log to your Account</text>
-              <input
-                placeholder="Email"
-                name="Email"
-                required
-                id="emailAddress"
-              />
-              <input
-                placeholder="Password"
-                required
-                type="password"
-                id="password"
-                name="Password"
-              />
-
-              <button type="submit" value="submit" color="primary">
-                Login
-              </button>
-            </div>
-          </form>
+    <div className="loggingmain">
+      <div className="rectangleBox">
+        <div className="rightElement">
+          <img src={Avatar} alt="Avatar" id="avatar" />
+          <h1>Welcome To Your Dashboard</h1>
+          <h2>
+            Your uploaded APIs will be displayed here once you login to your
+            account
+          </h2>
         </div>
       </div>
-    </React.Fragment>
+      <div className="form">
+        <form>
+          <div className="formElement">
+            <h2 id="text">Login to your Account</h2>
+            <input
+              placeholder="Email"
+              name="Email"
+              required
+              id="emailAddress"
+            />
+            <input
+              placeholder="Password"
+              required
+              type="password"
+              id="password"
+              name="Password"
+            />
+
+            <button
+              id="loggedbutton"
+              type="submit"
+              value="submit"
+              color="primary"
+            >
+              Login/Signup
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
